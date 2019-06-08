c25e0c4755f474b0e0f597e3eb9040bcbb0e24dd
Philippe van Amerongen <philippe.vanamerongen@gmail.com>
2019-06-08 17:08:40 +0200

    Add all the charts

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,95 @@
 <template>
     <div class="font-sans">
+        <!-- header -->
         <section id="header" class="-pb-1 h-40 md:h-56 text-2xl md:text-4xl">
-            <h1 class="text-center sm:text-left text-white pt-12 sm:pl-8 mx-4">Is Data Visualization worth it?</h1>
+            <h1 class="text-center sm:text-left text-white pt-12 sm:pl-8 mx-4 font-normal">Is Data Visualization worth
+                it?</h1>
         </section>
-        <!-- top section -->
-        <section class="flex my-2">
-            <div class="w-full sm:w-1/2 my-auto mx-4">
-                <p>Most jobs are affected by Data Visualization</p>
-                <highcharts :options="chartOptions"></highcharts>
-            </div>
-            <div class="w-full sm:w-1/2 my-auto mx-4">
-                <p> Data Visualization is a part of the job, most people were not hired for it</p>
-                <div>
-                    This is a graph
-                    <highcharts :options="chartOptions"></highcharts>
-                </div>
-            </div>
-        </section>
+
         <!-- section1 -->
-        <section class="h-64 bg-black text-white">
-
+        <section class="flex flex-wrap mb-2 mt-6">
+            <div class="w-full sm:w-1/2 px-4">
+                <h2 class="text-center sm:text-left text-xl h-24 ml-12">Most jobs are affected by Data
+                    Visualization</h2>
+                <highcharts :options="charts[0]"></highcharts>
+            </div>
+            <div class="w-full sm:w-1/2 px-4">
+                <h2 class="text-center sm:text-left text-xl h-24 ml-12"> Data Visualization is a part of the job, most
+                    people were not hired for it</h2>
+                <highcharts :options="charts[1]"></highcharts>
+            </div>
         </section>
+
         <!-- section2 -->
-        <section class="h-64 flex">
-            <div class="w-1/2">
-
+        <section class="bg-black text-white">
+            <div class="w-full sm:w-1/2 px-4">
+                <h2 class="text-center sm:text-left text-xl h-24 ml-12">Most jobs are affected by Data
+                    Visualization</h2>
+                <highcharts :options="charts[2]"></highcharts>
             </div>
-            <div class="w-1/2">
-
+            <div class="w-full sm:w-1/2 px-4">
+                <h2 class="text-center sm:text-left text-xl h-24 ml-12"> Data Visualization is a part of the job, most
+                    people were not hired for it</h2>
+                <highcharts :options="charts[3]"></highcharts>
             </div>
+        </section>
+
+        <!-- section3 -->
+        <section class="bg-black text-white">
+            <h2 class="text-center sm:text-left text-xl h-24 ml-12">Most jobs are affected by Data Visualization</h2>
+            <highcharts :options="charts[4]"></highcharts>
+        </section>
+
+
+        <!-- section4 -->
+        <section class="bg-black text-white">
+            <div class="w-full sm:w-1/3 px-4">
+                <h2 class="text-center sm:text-left text-xl h-24 ml-12">Most jobs are affected by Data
+                    Visualization</h2>
+                <highcharts :options="charts[5]"></highcharts>
+            </div>
+            <div class="w-full sm:w-1/3 px-4">
+                <h2 class="text-center sm:text-left text-xl h-24 ml-12">Most jobs are affected by Data
+                    Visualization</h2>
+                <highcharts :options="charts[6]"></highcharts>
+            </div>
+            <div class="w-full sm:w-1/3 px-4">
+                <h2 class="text-center sm:text-left text-xl h-24 ml-12"> Data Visualization is a part of the job, most
+                    people were not hired for it</h2>
+                <highcharts :options="charts[7]"></highcharts>
+            </div>
+        </section>
+
+        <!-- section5 -->
+        <section class="bg-black text-white">
+            <div class="w-full sm:w-1/2 px-4">
+                <h2 class="text-center sm:text-left text-xl h-24 ml-12">Most jobs are affected by Data
+                    Visualization</h2>
+                <highcharts :options="charts[8]"></highcharts>
+            </div>
+            <div class="w-full sm:w-1/2 px-4">
+                <h2 class="text-center sm:text-left text-xl h-24 ml-12"> Data Visualization is a part of the job, most
+                    people were not hired for it</h2>
+                <highcharts :options="charts[9]"></highcharts>
+            </div>
+        </section>
+
+        <!-- footer -->
+        <section class="bg-black text-white">
+            <h1 class="text-center mx-5 text-4xl"></h1>
         </section>
     </div>
 </template>
 
 <script>
 import {Chart} from 'highcharts-vue'
+import charts from './charts'
 
 export default {
     name: 'App',
     data() {
         return {
-            chartOptions: {
-                series: [{
-                    data: [1,2,3] // sample data
-                }]
-            }
+            charts: charts
         }
     },
     components: {
@@ -62,7 +107,8 @@ export default {
 #header::before {
     content: "";
     position: absolute;
-    top: -4px; left: -4px;
+    top: -4px;
+    left: -4px;
     width: calc(100% + 4px);
     height: calc(100% + 4px);
     background: url(./assets/datavis.jpg);
---
+++ src/charts/index.js
@@ -0,0 +1,23 @@
+import chart0 from './chart0.json'
+import chart1 from './chart1.json'
+import chart2 from './chart2.json'
+import chart3 from './chart3.json'
+import chart4 from './chart4.json'
+import chart5 from './chart5.json'
+import chart6 from './chart6.json'
+import chart7 from './chart7.json'
+import chart8 from './chart8.json'
+import chart9 from './chart9.json'
+
+export default [
+    chart0,
+    chart1,
+    chart2,
+    chart3,
+    chart4,
+    chart5,
+    chart6,
+    chart7,
+    chart8,
+    chart9
+]
