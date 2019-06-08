e592766d8c5682130d6254fcca5cfd3932f9beab
Philippe van Amerongen <philippe.vanamerongen@gmail.com>
2019-06-08 16:02:59 +0200

    Design

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <template>
     <div class="font-sans">
-        <section>
-            Hello
-            <div class="">asdasd</div>
+        <section id="header" class="-pb-1 h-40 md:h-56 text-2xl md:text-4xl">
+            <h1 class="text-center sm:text-left text-white pt-12 sm:pl-8 mx-4">Is Data Visualization worth it?</h1>
+        </section>
+        <!-- top section -->
+        <section class="flex my-2">
+            <div class="w-1/2 my-auto">
+                <p>Most jobs are affected by Data Visualization</p>
+                <div>
+                </div>
+            </div>
+            <div class="w-1/2 my-auto">
+                <p> Data Visualization is a part of the job, most people were not hired for it</p>
+                <div>
+                    This is a graph
+                </div>
+            </div>
+        </section>
+        <!-- section1 -->
+        <section class="h-64 bg-black text-white">
+
+        </section>
+        <!-- section2 -->
+        <section class="h-64 flex">
+            <div class="w-1/2">
+
+            </div>
+            <div class="w-1/2">
+
+            </div>
         </section>
     </div>
 </template>
@@ -19,4 +45,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#header {
+    @apply overflow-hidden shadow-xl;
+    position: relative;
+}
+
+#header::before {
+    content: "";
+    position: absolute;
+    top: -4px; left: -4px;
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    background: url(./assets/datavis.jpg);
+    background-size: auto 130%;
+    background-repeat: no-repeat;
+    background-position: center top;
+
+    filter: grayscale(100%) blur(2px) brightness(0.5);
+
+    @screen sm {
+        background-size: 100%;
+    }
+}
+
+#header > * {
+    position: relative;
+}
 </style>
